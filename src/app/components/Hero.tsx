64b5eaa4c1260e572806e2cfdbf104b6641595f5
Taofeek <ibrahimtaofeek005@gmail.com>
2024-06-21 15:49:35 +0100

    new section added in the home page

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,9 +4,16 @@ export default function Hero() {
             <h1 className="text-4xl font-bold text-center">
                 Find your next <br/> dream job
             </h1>
-            <p className="text-center text-gray-700">
+            <p className="text-center text-gray-600">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos assumenda cupiditate recusandae voluptas tempore quidem dolores eius officia dolorem provident quod sunt eveniet.
             </p>
+            <form>
+                <input 
+                    type="search" 
+                    className="border border-gray-400 w-full py-2 px-3 rounded-md" 
+                    placeholder="Search phrase.."/>
+                <button className="bg-blue-600 text-white py-2 px-4 rounded-md">Search</button>
+            </form>
         </section>
     )
 }

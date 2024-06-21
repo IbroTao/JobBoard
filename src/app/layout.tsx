bc9fb0675c565853e1aea07ecadc4d01266f3501
Taofeek <ibrahimtaofeek005@gmail.com>
2024-06-21 07:26:55 +0100

    login and post navs added to the dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header>
-          <div className="container flex justify-between py-4 px-6 mx-auto">
-            <Link href={"/"} className="font-bold">Job Board</Link>
-            <nav className="flex gap- *:bg-gray-200 *:py-2">
-            <Link href={'/login'}>Login</Link>
-            <Link href={'/new-listing'}>Post a Job</Link>
+          <div className="container flex items-center justify-between py-4 px-6 mx-auto">
+            <Link href={"/"} className="font-bold text-xl">Job Board</Link>
+            <nav className="flex gap-2 *:py-2 *:px-4 *:rounded-md">
+            <Link className="bg-gray-200" href={'/login'}>Login</Link>
+            <Link className="bg-blue-600 text-white" href={'/new-listing'}>Post a Job</Link>
             </nav>
           </div>
         </header>

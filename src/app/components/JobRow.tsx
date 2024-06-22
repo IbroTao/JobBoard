c25e44a581a1721a422394505076f0e0b4fa66a5
Taofeek <ibrahimtaofeek005@gmail.com>
2024-06-23 00:33:57 +0100

    new components added for job row

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -2,8 +2,12 @@ export default function JobRow() {
     return(
         <>
             <div className="bg-white p-6 rounded-md shadow-sm flex gap-4">
+                <div className="size-24">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="" />
+                </div>
                 <div>
-                    <img src="" alt="" />
+                    <div>Spotify</div>
+                    <div>Product Designer</div>
                 </div>
                 <div>jobs</div>
                 <div>created</div>

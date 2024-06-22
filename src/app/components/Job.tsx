c3771d110b68a54e795c754076de0520bd47efd2
Taofeek <ibrahimtaofeek005@gmail.com>
2024-06-22 23:35:10 +0100

    image added for jobs

--- src/app/components/Job.tsx
+++ src/app/components/Job.tsx
@@ -1,3 +1,5 @@
+import JobRow from "./JobRow";
+
 export default function Jobs() {
     return(
         <div className="bg-slate-200 py-4 rounded-xl">
@@ -5,10 +7,11 @@ export default function Jobs() {
                 <h2 className="font-bold mb-4">Recent Jobs</h2>
             </div>
             <div className="flex flex-col gap-4">
-                <div className="bg-white p-6 rounded-md shadow-sm">test job</div>
-                <div className="bg-white p-6 rounded-md shadow-sm">test job</div>
-                <div className="bg-white p-6 rounded-md shadow-sm">test job</div>
-                <div className="bg-white p-6 rounded-md shadow-sm">test job</div>
+                <JobRow/>
+                <JobRow/>
+                <JobRow/>
+                <JobRow/>
+                <JobRow/>
             </div>
         </div>
     )
--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,5 +1,13 @@
 export default function JobRow() {
     return(
-        
+        <>
+            <div className="bg-white p-6 rounded-md shadow-sm flex gap-4">
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>jobs</div>
+                <div>created</div>
+            </div>
+        </>
     )
 }

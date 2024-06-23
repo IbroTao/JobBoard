5b27a02422089a4f0a03e111870ea38a1b64e436
Taofeek <ibrahimtaofeek005@gmail.com>
2024-06-23 08:44:52 +0100

    added companies logo, job desc. and job time frame

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -2,16 +2,22 @@ export default function JobRow() {
     return(
         <>
             <div className="bg-white p-6 rounded-md shadow-sm flex gap-4">
-                <div className="size-24">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="" />
+                <div className="content-center">
+                    {/* <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="" /> */}
+                    <img 
+                        className="size-12"
+                        src="../images/me.jpg" alt="" />
                 </div>
-                <div>
+                <div className="grow">
                     <div>Spotify</div>
                     <div>Product Designer</div>
+                    <div>Remote &middot; New York, USA &middot; Full-time</div>
                 </div>
-                <div>jobs</div>
-                <div>created</div>
+                <div className="content-end text-sm text-gray-500">
+                    2 weeks ago
+                </div>
             </div>
         </>
     )
 }
+

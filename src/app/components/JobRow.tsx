6491ac0e8467adea8dbfeadd416a0b10dff763b2
Taofeek <ibrahimtaofeek005@gmail.com>
2024-06-24 16:22:14 +0100

    icon used from font awesome on the home page

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,8 +1,12 @@
+import { faHeart } from "@fortawesome/free-regular-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 export default function JobRow() {
     return(
         <>
             <div className="bg-white p-4 rounded-md shadow-sm relative">
-                <div className="absolute top-4 right-4">heart</div>
+                <div className="absolute top-2 right-4">
+                    <FontAwesomeIcon className="size-6 text-gray-500" icon={faHeart}/>
+                </div>
                 <div className="flex grow gap-4">
                     <div className="content-center">
                         {/* <img className="size-12" src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="" /> */}

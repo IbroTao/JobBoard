a9df20ef8b8a23a94344e55275d1ab4f8761d970
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-04 17:42:04 +0100

    job listing page: added inputs, button

--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -1,3 +1,5 @@
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getUser } from "@workos-inc/authkit-nextjs"
 
 export default async function NewLisitngPage() {
@@ -9,13 +11,19 @@ export default async function NewLisitngPage() {
             )}
             {!user && (
                 <div>
-                    <form action=""></form>
-                    <button className="bg-gray-200 px-4 py-2 rounded-md">
-                        Create a company
-                    </button>
+                    <h2 className="text-lg">Create a new company</h2>
+                    <p className="text-gray-500 text-sm mb-2">To create a job listing your first need to register a company</p>
+                    <form action="" className="flex gap-2">
+                        <input
+                            className="p-2 border border-gray-400 rounded-md"
+                            type="text" placeholder="company name"/>
+                        <button className="flex gap-2 bg-gray-200 px-4 py-2 rounded-md">
+                            Create a company 
+                            <FontAwesomeIcon className="h-6" icon={faArrowRight}/>
+                        </button>
+                    </form>
                 </div>
             )}
-            <div>{JSON.stringify(user)}</div>
         </div>
     )
 }

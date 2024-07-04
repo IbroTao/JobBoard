1ed541c73c23986da3470574d6787b3d13973fe9
Taofeek <ibrahimtaofeek005@gmail.com>
2024-07-04 13:44:20 +0100

    job listing page working

--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -1,5 +1,21 @@
-export default function NewLisitngPage() {
+import { getUser } from "@workos-inc/authkit-nextjs"
+
+export default async function NewLisitngPage() {
+    const {user} = await getUser();
     return(
-        <div>new listing</div>
+        <div className="container">
+            {user && (
+                <div>You need to be logged in to post a job</div>
+            )}
+            {!user && (
+                <div>
+                    <form action=""></form>
+                    <button className="bg-gray-200 px-4 py-2 rounded-md">
+                        Create a company
+                    </button>
+                </div>
+            )}
+            <div>{JSON.stringify(user)}</div>
+        </div>
     )
 }
